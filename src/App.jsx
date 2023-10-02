a76a271e5a48feb09784eb4f94b8412f4643b033
Rahiman <abdurrahiman100@gmail.com>
2023-10-02 19:33:16 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,14 @@ import DisplayUsers from "./DisplayUsers";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [users, setUsers] = useState([]);
-
   const submitClicked = (e) => {
     e.preventDefault();
-    const newId = uuidv4();
-    const newUser = {
-      name,
-      email,
-      id: newId,
-    };
-    setUsers((prevUsers) => [...prevUsers, newUser]);
-    setName("");
-    setEmail("");
-  };
+    const formData = new FormData(e.currentTarget); //this will refer to the <form>..</form>
+    const name = formData.get("name"); // o/p will be the text entered in the input.
+    console.log([...formData.entries()]); //this will generate the o/p of all the entries in the output field in the form of array
 
-  const delUser = (id) => {
-    const deletedUsersList = users.filter((eachUser) => {
-      return eachUser.id !== id;
-    });
-    console.log(deletedUsersList);
-    setUsers(deletedUsersList);
+    const newUser = Object.fromEntries(formData); // this will give the o/p as the object so that we can directly send the data
+    console.log(newUser);
   };
 
   return (
@@ -38,19 +23,24 @@ function App() {
           <label className="form-label" htmlFor="name">
             Name:-
           </label>
-          <input type="text" id="name" className="form-input" />
+          <input type="text" id="name" className="form-input" name="name" />
         </div>
         <div className="form-input-comp">
           <label className="form-label" htmlFor="email">
             Email:-
           </label>
-          <input type="email" id="email" className="form-input" />
+          <input type="email" id="email" className="form-input" name="email" />
         </div>
         <div className="form-input-comp">
           <label className="form-label" htmlFor="email">
             Password:-
           </label>
-          <input type="password" id="password" className="form-input" />
+          <input
+            type="password"
+            id="password"
+            className="form-input"
+            name="password"
+          />
         </div>
         <div className="submit-btn-cont">
           <button type="submit" className="submit-btn">
